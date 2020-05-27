8f76ebc4faf8b7e0fea8eca6f70217b68b858dff
ningshunqiang <920062955@qq.com>
2020-05-27 16:22:03 +0800

    refactor: 去除多余标签

--- src/pages/cache/components/EditCache.tsx
+++ src/pages/cache/components/EditCache.tsx
@@ -42,48 +42,46 @@ const EditCache: SFC<EditCacheModalProps> = ({
   };
 
   return (
-    <div>
-      <Modal
-        cancelText="取消"
-        forceRender
-        okText="保存"
-        title="编辑缓存"
-        visible={visible}
-        width={600}
-        onCancel={onCancel}
-        onOk={handleSubmit}
+    <Modal
+      cancelText="取消"
+      forceRender
+      okText="保存"
+      title="编辑缓存"
+      visible={visible}
+      width={600}
+      onCancel={onCancel}
+      onOk={handleSubmit}
+    >
+      <Form
+        form={form}
+        labelCol={{ span: 7 }}
+        wrapperCol={{ span: 13 }}
+        onFinish={handleFinish}
       >
-        <Form
-          form={form}
-          labelCol={{ span: 7 }}
-          wrapperCol={{ span: 13 }}
-          onFinish={handleFinish}
+        <Form.Item
+          initialValue={false}
+          label="状态"
+          name="enable"
+          valuePropName="checked"
         >
-          <Form.Item
-            initialValue={false}
-            label="状态"
-            name="enable"
-            valuePropName="checked"
-          >
-            <Switch defaultChecked={false} />
-          </Form.Item>
-          <Form.Item
-            label="名称"
-            name="name"
-            rules={[{ required: true, message: "名称" }]}
-          >
-            <Input placeholder="请输入" />
-          </Form.Item>
-          <Form.Item
-            label="标识"
-            name="key"
-            rules={[{ required: true, message: "key" }]}
-          >
-            <Input placeholder="请输入" />
-          </Form.Item>
-        </Form>
-      </Modal>
-    </div>
+          <Switch defaultChecked={false} />
+        </Form.Item>
+        <Form.Item
+          label="名称"
+          name="name"
+          rules={[{ required: true, message: "名称" }]}
+        >
+          <Input placeholder="请输入" />
+        </Form.Item>
+        <Form.Item
+          label="标识"
+          name="key"
+          rules={[{ required: true, message: "key" }]}
+        >
+          <Input placeholder="请输入" />
+        </Form.Item>
+      </Form>
+    </Modal>
   );
 };
 
